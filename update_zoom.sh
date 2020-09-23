0b75bef7eb10bc889094ef2c1e97db15fa9badcb
Matthew Zagaja <mzagaja@mapc.org>
2020-09-23 00:53:35 +0000

    Add update zoom script
    This script run with ./update_zoom.sh ‘new_url’ should speed up updating the website.

--- update_zoom.sh
+++ update_zoom.sh
@@ -3,3 +3,7 @@
 sed -i.tmp -e "s/redirect_to.*/redirect_to: $1/g" _rooms/cfb.md
 # update ./_config.yml
 sed -i.tmp -e "s/zoom_url:.*/zoom_url: $1/g" _config.yml
+
+git add .
+git commit -m 'Update Zoom Url'
+git push origin master
