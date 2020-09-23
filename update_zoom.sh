3cc7b4cf06aab156cb1135a8a1a40d0dd74b45c6
Matthew Zagaja <mzagaja@mapc.org>
2020-09-23 00:53:07 +0000

    Add script to make updating Zoom url quicker
    ./update_zoom.sh ‘new_url’ should speed things up in future weeks.

---
+++ update_zoom.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# update _rooms/cfb.md with new url from first argument
+sed -i.tmp -e "s/redirect_to.*/redirect_to: $1/g" _rooms/cfb.md
+# update ./_config.yml
+sed -i.tmp -e "s/zoom_url:.*/zoom_url: $1/g" _config.yml
